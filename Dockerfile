1fa814682dfb392492edf1acb43941c797480420
eduardoalumin <eduardo.alumin@gmail.com>
2023-10-17 09:23:56 -0600

    test

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM tomcat:8.0-alpine
-LABEL maintainer="deepak@softwareyoga.com"
 
-ADD sample.war /usr/local/tomcat/webapps/
+ADD cardif.war /usr/local/tomcat/webapps/
 
 EXPOSE 8080
 CMD ["catalina.sh", "run"]
---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t tomcat-test:1.0 .
+#docker tag tomcat-test:1.0 gcr.io/airmedia/tomcat-test:1.0
+#docker push gcr.io/airmedia/tomcat-test:1.0
+#gcloud container images list-tags gcr.io/airmedia/tomcat-test
